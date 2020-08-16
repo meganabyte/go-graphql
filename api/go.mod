f3488eb697b835c69fda855b1df87dd6ca437671
meganabyte <megbobba@gmail.com>
2020-08-15 23:03:36 -0700

    add graphql handler

--- api/go.mod
+++ api/go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/gorilla/securecookie v1.1.1
 	github.com/gorilla/sessions v1.2.0
 	github.com/graphql-go/graphql v0.7.9
+	github.com/graphql-go/handler v0.2.3
 	go.mongodb.org/mongo-driver v1.4.0
 	golang.org/x/oauth2 v0.0.0-20200107190931-bf48bf16ab8d
 )
--- api/main.go
+++ api/main.go
@@ -4,51 +4,49 @@ import (
 	"fmt"
 	"net/http"
 	"api/handlers"
-	/*
 	"github.com/graphql-go/graphql"
-	"log"
-	"encoding/json"
+	"github.com/graphql-go/handler"
 	"api/queries"
 	"api/mutations"
-	*/
 )
 
-/*
 var schema, _ = graphql.NewSchema(graphql.SchemaConfig{
 	Query: queries.RootQuery,
 	Mutation: mutations.RootMutation,
 })
-*/
 
 
 func main() {
+
+	h := handler.New(&handler.Config{
+		Schema: &schema,
+		GraphiQL:true,
+		Pretty: true,
+	})
 
 	http.Handle("/client/", http.StripPrefix("/client/", http.FileServer(http.Dir("../client"))))
 	http.HandleFunc("/", handlers.HandleMain)
 	http.HandleFunc("/login", handlers.HandleLogin)
 	http.HandleFunc("/callback", handlers.HandleCallback)
 	http.HandleFunc("/dashboard", handlers.HandleDashboard)
+	http.Handle("/graphql", disableCors(h))
 	http.HandleFunc("/logout", handlers.HandleLogout)
 	fmt.Println(http.ListenAndServe(":8080", nil))
 
-	/*
-	// Query or mutation
-	request := `
-			{
-				student(Email:"test@gmail.com") {
-					Email
-				}
-			}
-	`
-	params := graphql.Params{Schema: schema, RequestString: request}
-	r := graphql.Do(params)
-	if len(r.Errors) > 0 {
-		log.Fatalf("failed to execute graphql operation, errors: %+v", r.Errors)
-	}
-	rJSON, _ := json.Marshal(r)
-	fmt.Printf("%s \n", rJSON)
-	*/
+}
 
+func disableCors(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+	w.Header().Set("Access-Control-Allow-Headers", "Accept, Authorization, Content-Type, Content-Length, Accept-Encoding")
+	if r.Method == "OPTIONS" {
+	  w.Header().Set("Access-Control-Max-Age", "86400")
+	  w.WriteHeader(http.StatusOK)
+	  return
+	}
+	h.ServeHTTP(w, r)
+   })
 }
 
 
